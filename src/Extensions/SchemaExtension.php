26b861b0beaf9d004f45dac6b3147b61cce173f4
Sunny Side Up <github@sunnysideup.co.nz>
2026-05-22 21:56:27 +1200

    Update SchemaExtension.php

--- src/Extensions/SchemaExtension.php
+++ src/Extensions/SchemaExtension.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * SchemaExtension.php
  *
@@ -11,11 +12,13 @@ namespace Broarm\Schema;
 use Broarm\Schema\SchemaBuilder;
 use SilverStripe\Control\Controller;
 use SilverStripe\Core\Config\Config;
-use SilverStripe\Core\Convert;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\HeaderField;
+use SilverStripe\Forms\LiteralField;
 use SilverStripe\ORM\DataExtension;
-use SilverStripe\ORM\FieldType\DBVarchar;
+use SilverStripe\Versioned\Versioned;
 use SilverStripe\View\Requirements;
-use Spatie\SchemaOrg\BaseType;
+use Sunnysideup\ArrayToUl\View\ExpandableArrayList;
 
 /**
  * SchemaExtension
@@ -36,32 +39,32 @@ class SchemaExtension extends DataExtension
     public function MetaTags(&$tags)
     {
         $curr = Controller::curr();
-        if($curr) {
+        if ($curr) {
             $request = Controller::curr()->getRequest();
-            if($request) {
-                if($request->isAjax()) {
+            if ($request) {
+                if ($request->isAjax()) {
                     return;
                 }
-                if($request->param('Action')) {
+                if ($request->param('Action')) {
                     return;
                 }
                 $postVars = $request->postVars();
-                if(! empty($postVars)) {
+                if (! empty($postVars)) {
                     return;
                 }
                 $getVars = $request->getVars();
-                if(! empty($getVars)) {
-                    if(count($_GET) > 1) {
+                if (! empty($getVars)) {
+                    if (count($_GET) > 1) {
                         return;
                     } else {
-                        if(!empty(array_intersect(array_keys($getVars), Config::inst()->get(static::class, 'exempted_get_vars')))) {
+                        if (!empty(array_intersect(array_keys($getVars), Config::inst()->get(static::class, 'exempted_get_vars')))) {
                             return;
                         }
                     }
                 }
                 $schemaBuilders = $this->getSchemasOrg();
                 /** @var SchemaBuilder $schemaBuilder */
-                foreach($schemaBuilders as $schemaBuilder) {
+                foreach ($schemaBuilders as $schemaBuilder) {
                     $this->appendSchemaOrg($tags, $schemaBuilder);
                 }
             }
@@ -80,7 +83,7 @@ class SchemaExtension extends DataExtension
         if ($schemaBuilder) {
             $owner = $this->getOwner();
             $array = $schemaBuilder->getSchemaFromCache($owner);
-            if(!empty($array)) {
+            if (!empty($array)) {
                 $string = str_replace('$', '&#36;', json_encode($array, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE));
                 Requirements::insertHeadTags(
                     '<script type="application/ld+json">' . $string . '</script>',
@@ -95,24 +98,98 @@ class SchemaExtension extends DataExtension
      *
      * @return SchemaBuilder[]
      */
-    public function getSchemasOrg()
+    public function getSchemasOrg(): array
     {
         $array = [];
-        $schemas = array_filter($this->owner->config()->get('active_schema'));
+        $schemas = array_filter($this->getOwner()->config()->get('active_schema'));
         foreach ($schemas as $schema) {
-            if(class_exists($schema)) {
+            if (class_exists($schema)) {
                 $schemaBuilder = new $schema();
                 if ($schemaBuilder instanceof SchemaBuilder) {
                     $array[$schema] = $schemaBuilder;
                 }
+            } else {
+                user_error('Schema class ' . $schema . ' does not exist', E_USER_WARNING);
             }
         }
         return $array;
     }
 
+    public function getSchemaOrgHumanReadable(): array
+    {
+        $list = [];
+        $schemaBuilders = $this->getSchemasOrg();
+        foreach ($schemaBuilders as $schemaBuilderObject) {
+            $v = $schemaBuilderObject->getInfoLink($this->getOwner());
+            if ($v) {
+                $list[] = $v;
+            }
+        }
+        return $list;
+    }
+
     public function onAfterWrite()
     {
+        $owner = $this->getOwner();
+        if (! $owner->hasExtension(Versioned::class)) {
+            SchemaBuilder::clear_schema_cache();
+        }
+    }
+
+    public function onAfterPublish()
+    {
         SchemaBuilder::clear_schema_cache();
+    }
+
+    public function updateCMSFields(FieldList $fields)
+    {
+
+        $fields->addFieldsToTab(
+            'Root.Schema',
+            [
+                LiteralField::create(
+                    'SchemaDotOrgTestLinkNice',
+                    '<p><h2>Review actual data</h2><a href="' . $this->getSchemaTestLink() . '" target="_blank" rel="noopener noreferrer">Review Schema for ' . $this->getOwner()->Title . '</a></p>'
+                ),
+                LiteralField::create(
+                    'SchemaDotOrgPrintOutTypes',
+                    '<h2 style="margin-top: 20px">Schema Types</h2>' . ExpandableArrayList::create($this->getSchemaOrgHumanReadable())->setAllowHtmlAsIs(true)->forTemplate()
+                ),
+                LiteralField::create(
+                    'SchemaDotOrgPrintOutDetails',
+                    '<h2 style="margin-top: 20px">List of Actual Data</h2><pre>' . json_encode($this->getSchemaOrgTestData(), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) . '</pre>'
+                ),
+            ]
+        );
+    }
+
+    public function getSchemaTestLink(): string
+    {
+        return 'https://validator.schema.org/#' . urlencode($this->getOwner()->AbsoluteLink());
+
+    }
+
+    protected function getSchemaOrgTestData(): array
+    {
+        $owner = $this->getOwner();
+        $all = [];
+        $schemaBuilders = $this->getSchemasOrg();
+        /** @var SchemaBuilder $schemaBuilder */
+        foreach ($schemaBuilders as $schemaBuilder) {
+            if ($schemaBuilder) {
+                $array = $schemaBuilder->getSchema($owner);
+                if (!empty($array)) {
+                    $all[] = $array;
+
+                }
+            }
+        }
+        return $all;
+    }
+
+    protected function todo()
+    {
+
     }
 
 
